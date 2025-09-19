ab7a1de9a7509e0ed5b146b9b167fec623b89e6e
Prasad Kandalkar <pkkandalkar19@gmail.com>
2025-09-19 14:47:16 +0530

    First commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 132455
+let accountEmail = "pkkar1904@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nashik"
+let accountState
+
+//accountId = 234253
+accountEmail = "pkkr23@ex.com"
+
+console.log(accountEmail);
+
+/* 
+prefer not to use var. 
+because issue of the block space and functional space.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(1+3) // we are using node.js so cant directly use alert as in browser console
+
+let name = "Prasad"
+let age 
+let isLoggedIn = false
+
+// number: number data up to 2 to power 53
+// bigint: very large number
+// string: characters inside ""
+// boolean: true and false
+// null: standalone value empty value
+// undefined: unknown
+// symbol: uniqeness
+// object:
+
+
+console.log(typeof "prasad");
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof age)
+console.log(typeof undefined);
+
+
+
+
