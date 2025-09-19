338e2bae6b6b6106ca02341b4c0d3a8ee9e9e8c8
Prasad Kandalkar <pkkandalkar19@gmail.com>
2025-09-19 17:09:43 +0530

    conversion of datatype and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,18 +1,36 @@
-let score = "33acd"
+// let score = "33acd"
 
-console.log(typeof score) 
+// console.log(typeof score) 
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber) 
-console.log(valueInNumber);
-console.log(typeof NaN);
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber);
+// console.log(typeof NaN);
 
-let scoreA = null
-let valueInNumberA = Boolean(scoreA)
-let valueInNumberB = String(scoreA)
-let valueInNumberC = Number(scoreA)
+// let scoreA = null
+// let valueInNumberA = Boolean(scoreA)
+// let valueInNumberB = String(scoreA)
+// let valueInNumberC = Number(scoreA)
 
-console.log(valueInNumberA);
-console.log(valueInNumberB);
-console.log(valueInNumberC);
+// console.log(valueInNumberA);
+// console.log(valueInNumberB);
+// console.log(valueInNumberC);
+
+
+// Operations 
+
+
+let value = 4
+let negValue = -value
+
+// arithmatic operatores: +, -, *, /, %, ** (similar to python)
+
+console.log("1"+2+2); //==> 122
+console.log(1+2+"2"); //==> 32
+
+// the operation gives the priority to the first element.
+
+let A = 2
+A++; // increment will be after usage
+++A;// increment will be before usage
