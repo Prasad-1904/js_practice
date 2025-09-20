f946f17f70bd74fcd553931bffa66af0f81ba250
Prasad Kandalkar <pkkandalkar19@gmail.com>
2025-09-20 12:18:47 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// comparison operators: <, >, <=, >=, ==, != similar to python
+
+console.log("1"<2);
+console.log("1">2);
+
+// In above cases the Javascript convets the string into number
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// in the line 10 the js converts the null into zero or NaN. Dont use this type of comparison anywhere
