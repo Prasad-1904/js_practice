2a77a673d0c34ee509bff72e10bacbdac2db2989
brokeraway-prod <brokeraway@gamil.com>
2025-09-22 17:00:45 +0530

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -2,11 +2,21 @@
 
 console.log("1"<2);
 console.log("1">2);
+console.log();
+
 
 // In above cases the Javascript convets the string into number
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+console.log();
 
 // in the line 10 the js converts the null into zero or NaN. Dont use this type of comparison anywhere
+
+// in js '===' is used for strict comaparison. it also checks the datatype of the comparatives. 
+// here no coversions will be done.
+
+console.log("1"==1);
+console.log("1"===1);
+
