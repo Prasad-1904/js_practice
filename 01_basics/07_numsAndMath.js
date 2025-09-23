78d5bd34a1c9826ca478f1ab71ceee0b214a86ba
brokeraway-prod <brokeraway@gamil.com>
2025-09-23 12:48:57 +0530

    Numbers and Math

---
+++ 01_basics/07_numsAndMath.js
@@ -0,0 +1,60 @@
+const score = 300
+
+const balance = new Number(4000)
+
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length); // convets num to string and length of the number
+// console.log(balance.toFixed(2)); // for decimal points
+
+const precizeNum = 323.798423542534
+
+// toPrecision method can round the num to the given input.
+// note that the length of the num will be input num
+// console.log(precizeNum.toPrecision(5)); 
+// console.log(precizeNum.toPrecision(4));
+// console.log(precizeNum.toPrecision(3));
+// console.log(precizeNum.toPrecision(2));
+
+const numsA = 100000000
+
+// console.log(numsA.toLocaleString())
+// console.log(numsA.toLocaleString('en-IN'))
+
+
+// Maths
+console.log(Math);
+console.log(Math.PI);
+
+// E: 2.718281828459045
+// LN10: 2.302585092994046
+// LN2: 0.6931471805599453
+// LOG10E: 0.4342944819032518
+// LOG2E: 1.4426950408889634
+// PI: 3.141592653589793
+// SQRT1_2: 0.7071067811865476
+// SQRT2: 1.4142135623730951
+// ​
+
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(2,43,6,8,90));
+console.log(Math.max(3, 45, 65, 77,23))
+
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 19
+const max = 30
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
+
+
+
+
+
+
